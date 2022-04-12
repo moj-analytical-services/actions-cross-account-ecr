75f441ddb85ac90341b50a66377f8d06896cdb1e
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2022-04-12 15:06:24 +0100

    Update action to allow user to supply custom repository policy JSON.

--- generate_policy.py
+++ generate_policy.py
@@ -1,13 +1,15 @@
 import os
 import json
 
-account_id = os.getenv("ACCOUNT_ID")
-principals = [
-    f"arn:aws:iam::{account_id}:role/airflow-dev-node-instance-role",
-    f"arn:aws:iam::{account_id}:role/airflow-prod-node-instance-role",
-]
+policy_json = os.getenv("POLICY_JSON")
+if policy_json is not None:
+    try:
+        input_json = json.load(open(policy_json))
+    except Exception as e:
+        print("Failed to read policy json, due to the following error: \n")
+        print(e)
+else:
+    input_json = json.load(open("./actions-cross-account-ecr/standard_policy.json"))
 
-input_json = json.load(open("./actions-cross-account-ecr/standard_policy.json"))
-input_json["Statement"][0]["Principal"]["AWS"] = principals
-with open("./actions-cross-account-ecr/generated_policy.json", "w") as f:
+with open("./actions-cross-account-ecr/final_policy.json", "w") as f:
     output_json = json.dump(input_json, f, indent=4)

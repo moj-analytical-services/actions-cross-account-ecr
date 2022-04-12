f237dff20b2d68426dc2187a6ce9b737641dc14e
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2022-04-12 16:29:39 +0100

    Update action with fallback condition

--- generate_policy.py
+++ generate_policy.py
@@ -2,12 +2,14 @@ import os
 import json
 
 policy_json = os.getenv("POLICY_JSON")
-if policy_json is not None:
+if len(policy_json) != 0:
     try:
         input_json = json.load(open(policy_json))
     except Exception as e:
         print("Failed to read policy json, due to the following error: \n")
         print(e)
+        print("\n Applying standard policy instead!")
+        input_json = json.load(open("./actions-cross-account-ecr/standard_policy.json"))
 else:
     input_json = json.load(open("./actions-cross-account-ecr/standard_policy.json"))
 
